7b60ff1a1e6d44c7ee7b4cefeda1d2232398c7b0
mdarf <mdarfinji45@gmail.com>
2024-05-05 13:01:23 +0800

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import ProductList from './features/ProductList/Product-list';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-     
+      
+      <Navbar></Navbar>
        <ProductList></ProductList>
     </div>
   );
